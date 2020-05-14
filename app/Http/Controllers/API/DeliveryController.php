6c1865aadd62d8a295c63a96d36cecb605e04107
Ochieng424 <ochiengderrick424@gmail.com>
2020-05-14 11:02:23 +0300

    working

--- app/Http/Controllers/API/DeliveryController.php
+++ app/Http/Controllers/API/DeliveryController.php
@@ -17,36 +17,39 @@ class DeliveryController extends Controller
      */
     public function index()
     {
-       return Delivery::where('Rider_id',auth()->user()->id)->with('order')->get();
+        return Delivery::where('Rider_id', auth()->user()->id)->with('order')->get();
     }
-     public function rider($id){
-        $order_id = order::where('orderNo', $id)->value('id');
-        $user_id = Delivery::where('Order_id',$order_id)->value("Rider_id");
-        return User::where('id',$user_id)->with('ride')->firstOrFail();
-     }
 
-     public function shopInfo($id){
-         return order::where('orderNo', $id)->with('items.product.files')->with('customer')->with('coupon')->firstOrFail();
-     }
+    public function rider($id)
+    {
+        $order_id = order::where('orderNo', $id)->value('id');
+        $user_id = Delivery::where('Order_id', $order_id)->value("Rider_id");
+        return User::where('id', $user_id)->with('ride')->firstOrFail();
+    }
+
+    public function shopInfo($id)
+    {
+        return order::where('orderNo', $id)->with('items.product.files')->with('customer')->with('coupon')->firstOrFail();
+    }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request, $id)
     {
-        $this->validate($request,[
-            'rider' =>'required|integer'
+        $this->validate($request, [
+            'rider' => 'required|integer'
         ]);
-        $customer_id  = order::where('orderNo',$id)->value('customer_id');
-        $order_id = order::where('orderNo',$id)->value('id');
+        $customer_id = order::where('orderNo', $id)->value('customer_id');
+        $order_id = order::where('orderNo', $id)->value('id');
         $delivery = new Delivery();
         $delivery->Rider_id = $request->rider;
-        $delivery->Customer_id =$customer_id;
+        $delivery->Customer_id = $customer_id;
         $delivery->Order_id = $order_id;
-        $delivery -> save();
+        $delivery->save();
 
         $order = order::where('orderNo', $id)->firstOrFail();
         $order->status = "in-transit";
@@ -61,7 +64,7 @@ class DeliveryController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -72,8 +75,8 @@ class DeliveryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -84,7 +87,7 @@ class DeliveryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
