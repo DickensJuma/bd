dc2dc572c803569321b16cda9b31bccc7ca59aed
shemnduati <nduatishem@gmail.com>
2020-08-19 16:21:01 +0300

    blank

--- app/User.php
+++ app/User.php
@@ -94,8 +94,5 @@ class User extends Authenticatable implements JwtSubject
             'phone_verified_at' => $this->freshTimestamp(),
         ])->save();
     }
-    public function receivesBroadcastNotificationsOn()
-{
-    return 'shipment.User.' . $this->id;
-}
+    
 }
