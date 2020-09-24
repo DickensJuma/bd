204b7de7cd583d57e1d456b9c0b8504cda9273da
shemnduati <nduatishem@gmail.com>
2020-09-24 12:05:08 +0300

    done sms notification for the retailer/wholesaler after an order has been placed

--- app/Http/Controllers/API/OrdersController.php
+++ app/Http/Controllers/API/OrdersController.php
@@ -8,12 +8,14 @@ use App\Events\NewShipping;
 use App\FcmToken;
 use App\Helpers\FCM\RiderNotification;
 use App\Helpers\Payment\Mpesa;
+use App\Helpers\VasSms\Vas;
 use App\Http\Controllers\Controller;
 use App\order;
 use App\orderItem;
 use App\Product;
 use App\Rating;
 use App\Shipment;
+use App\User;
 use Illuminate\Http\Request;
 use Auth;
 
@@ -117,6 +119,7 @@ class OrdersController extends Controller
                 $shipment->save();
                 $total = 0;
 
+
                 foreach ($request->cart as $item) {
                     if ($item['product']['user_id'] == $id) {
                         $orderItem = new orderItem();
@@ -132,6 +135,11 @@ class OrdersController extends Controller
                 $shipment->total = $total;
                 $shipment->update();
                 broadcast(new NewShipping($shipment));
+
+                $phone = User::where('id', $id)->value('phone');
+                $message = 'New order has been placed with ORDERNO: ' . $shipment->shipmentId;
+                 Vas::send_sms($phone,$message);
+                ;
             }
 
             // Pay
