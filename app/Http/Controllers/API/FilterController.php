9c19f43a1606add1b354aed175bff5d2099ccbf7
Ochieng424 <ochiengderrick424@gmail.com>
2020-12-02 23:17:51 +0300

    mega search

--- app/Http/Controllers/API/FilterController.php
+++ app/Http/Controllers/API/FilterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Brand;
 use App\Http\Controllers\Controller;
+use App\Product;
 use App\SubCategory;
 use Illuminate\Http\Request;
 
@@ -11,8 +12,8 @@ class FilterController extends Controller
 {
     public function subCategories(Request $request)
     {
-        $category_id = $request->input('category');
-        $sub = $request->input('sub');
+        $category_id = $request->query('category');
+        $sub = $request->query('sub');
 
         if ($category_id) {
             $sub_categories = SubCategory::orderBy('name')->where('product_category_id', $category_id)->get();
@@ -20,22 +21,33 @@ class FilterController extends Controller
             $sub_categories = SubCategory::orderBy('name')->get();
         }
 
-        if ($category_id && !$sub) {
-            $brands = Brand::orderBy('name')->with(['subcategory', function ($query) use ($category_id) {
+        if ($category_id) {
+            $brands = Brand::orderBy('name')->whereHas('subcategory', function ($query) use ($category_id) {
                 return $query->where('product_category_id', $category_id);
-            }])->get();
+            })->get();
         }
         if ($sub) {
             $brands = Brand::orderBy('name')->where('sub_category_id', $sub)->get();
         }
 
-        if (!$category_id && !$sub) {
-            $brands = Brand::orderBy('name')->get();
+
+        $query = Product::query();
+        if ($request->query('category')){
+            $query->where('category_id', $request->query('category'));
+        }
+
+        if ($request->query('brand')){
+            $query->where('brand_id', $request->query('brand'));
+        }
+
+        if ($request->query('sub')){
+            $query->where('sub_category_id', $request->query('sub'));
         }
 
         return response()->json([
             'sub' => $sub_categories,
-            'brands' => $brands
+            'brands' => $brands,
+            'products' => $query->get()
         ], 200);
     }
 }
